497ec0eea6261458a523c09f803f8525255d7f5d
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-05 13:19:11 +0200

    Add search functionality to MainContent and update UI components

--- aurix/src/components/MainContent.tsx
+++ aurix/src/components/MainContent.tsx
@@ -1,19 +1,33 @@
-import React from "react";
+"use state";
+import { useState } from "react";
 import Search from "./main/Search";
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Calendar } from "./ui/calendar";
 import { Checkbox } from "./ui/checkbox";
 import { Slider } from "./ui/slider";
+import { Input } from "./ui/input";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { urls } from "@/lib/urls";
 
 function MainContent() {
   const results = 10;
 
+  const [query, setQuery] = useState("");
+
+  const { mutate: search, isPending: loading } = useMutation({
+    mutationKey: ["search"],
+    mutationFn: async () => {
+      const res = await axios.get(`${urls.backendUrl}/api/jobs?q=${query}`) as {data: {jobs?: any[]}};
+    }
+  })
+
   return (
-    <article className="flex gap-2 items-center w-full p-8">
+    <article className="flex gap-8 justify-center w-full p-8">
       {/* Filters sidebar */}
       <aside className="w-1/4">
-        <article className="rounded-md border border-gray-200">
+        <article className="rounded-2xl border border-gray-200">
           <article className="border-b border-gray-200 p-4 flex justify-between items-center">
             <h2 className="font-semibold">Filters</h2>
 
@@ -22,18 +36,18 @@ function MainContent() {
             </Button>
           </article>
 
-          <article className="flex flex-col gap-8 items-center p-4">
-            <article>
+          <article className="flex flex-col gap-8 justify-center p-4">
+            <article className="flex flex-col gap-2">
               <Label>Date Posted</Label>
-              <Calendar />
+              <Input type="date" />
             </article>
 
-            <article>
+            <article className="flex flex-col gap-2">
               <Label>Price Range</Label>
               <Slider defaultValue={[0]} max={5000} step={10} />
             </article>
 
-            <article>
+            <article className="flex flex-col gap-2">
               <Label>Platforms</Label>
               <article className="flex gap-4 items-center">
                 <article className="flex gap-2 items-center">
@@ -63,7 +77,7 @@ function MainContent() {
 
       {/* Main content */}
       <main className="w-3/4">
-        <h3 className="text-2xl">{results} Results  </h3>
+        <h3 className="text-xl">{results} Results</h3>
         <Search />
       </main>
     </article>
