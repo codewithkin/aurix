44ebd3241bbce22b9d922a037540fdc46a0f5cf1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-05 10:10:01 +0200

    Refactor Home component layout for improved styling and structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default async function Home() {
     <Topbar />
 
     {/* Header */}
-    <article className='px-4 py-8 border-y bg-slate-300 border-slate-600'>
-      <article>
+    <article className='p-16 border-y bg-gray-100 border-slate-400'>
+      <article className='flex flex-col gap-4 justify-center md:max-w-6xl'>
         <h2 className='font-semibold text-3xl'>Explore gigs across <span className="text-sky-700">the entire internet</span></h2>
         <p className="text-lg text-gray-600">
           Discover freelance gigs from top platforms like Upwork, AngelList, and Fiverr, all in one place. Whether you're a web developer, designer, or marketer, find opportunities that match your skills and interests. Start exploring today and take the next step in your freelance career.
