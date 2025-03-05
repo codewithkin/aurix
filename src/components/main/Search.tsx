3f1b4c75bb9ba210d927c63c07d76a3f96a071f0
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-05 10:22:21 +0200

    Enhance layout and styling in MainContent and Search components for improved user experience

--- src/components/main/Search.tsx
+++ src/components/main/Search.tsx
@@ -6,12 +6,13 @@ function Search() {
   return (
     <article className="flex gap-2 items-center w-full">
       <Input 
-        className='p-6 focus-visible:ring-sky-400 focus-visible:ring-1 focus-visible:outline-none focus-visible:border-0 rounded-lg'
+        className='py-6 px-4 focus-visible:ring-sky-400 focus-visible:ring-1 focus-visible:outline-none focus-visible:border-0 rounded-lg'
         type='search'
+        placeholder='Search for a gig keyword...'
       />
 
       <Button variant="default" className='bg-sky-400 text-white p-6'>
-        Search
+        Find Gigs
       </Button>
     </article>
   )
