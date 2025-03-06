7b1c36cb19ebb54898e0a8d7b8006b0025ce22d8
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-06 08:42:20 +0200

    Refactor index.ts to improve type imports and formatting

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import morgan from "morgan";
 import routes from "./routes";
 import upworkCrawler from './scrapers/upwork';
 
-const app: Express = express();
+const app = express();
 const port: number = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 
 // Middleware
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("combined"));
 
-app.get('/', (_req: Request , res: Response ) => {
+app.get('/', (_req: Request, res: Response) => {
     res.send('Hey there!');
 });
 
