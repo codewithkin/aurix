006bac843b247f78c458549fa6c5036e26e6bd6c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-06 10:36:53 +0200

    Add morgan middleware for logging and configure express to parse URL-encoded data

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import router from './routes/jobs.js';
+import morgan from "morgan";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("combined"))
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Express server!');
