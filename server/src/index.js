30ed9768dc320e2819b0dd8e0ffd5f080309f421
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-06 11:26:27 +0200

    Add CORS middleware to enable cross-origin requests

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './routes/jobs.js';
 import morgan from "morgan";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 8080;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("combined"))
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Welcome to the Express server!');
