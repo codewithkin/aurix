8d63530d1a2c2e28230d4b54efbd94bac1fe94da
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-07 12:04:01 +0200

    Refactor job retrieval to combine Reddit and Upwork scrapers and implement search filtering

--- server/src/controllers/getJobs.js
+++ server/src/controllers/getJobs.js
@@ -1,19 +1,23 @@
+import RedditScraper from "../lib/scrapers/reddit.js";
 import UpworkScraper from "../lib/scrapers/upwork.js";
 
 export default async function GetJobs (req, res) {
     try {
         // Get the user's search query (if any);
         const {q} = req.query;
-        let jobs;
+        const jobs = [];
 
-        if(q) {
-            jobs = await UpworkScraper(q);
+        const redditJobs = await RedditScraper();
+        const upworkJobs = await UpworkScraper();
 
-            return res.json(jobs);
-        }
- 
-        if(!jobs) {
-            jobs = await UpworkScraper();
+        for (let job of [...redditJobs, ...upworkJobs]) {
+            if (q) {
+                if (job.title.toLowerCase().includes(q.toLowerCase())) {
+                    jobs.push(job);
+                }
+            } else {
+                jobs.push(job);
+            }
         }
 
         return res.json(jobs);
