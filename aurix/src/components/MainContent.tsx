d376c3cccb4b0a7a40c8e1428dd1cea68a526ab1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-07 12:03:47 +0200

    Add null check for job in MainContent component

--- aurix/src/components/MainContent.tsx
+++ aurix/src/components/MainContent.tsx
@@ -100,6 +100,7 @@ function MainContent({jobs, fetching} : {jobs: any, fetching: boolean}) {
             {/* Jobs Cards */}
             <article className="flex flex-col gap-4 my-4">
               {jobs?.map((job: any, index: number) => (
+                job &&
                 <Card key={index}>
                     <CardContent>
                       <CardHeader>
