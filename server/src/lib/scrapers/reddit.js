5659a5a703d856195a6a228317f5d80c02beec71
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-07 10:26:16 +0200

    Fix Reddit scraper to ensure proper return after processing job elements

--- server/src/lib/scrapers/reddit.js
+++ server/src/lib/scrapers/reddit.js
@@ -31,6 +31,8 @@ export default async function RedditScraper() {
                                 date: el.querySelectorAll(".whitespace-nowrap")[1].textContent.replace(/\s+/g, ' ').trim(),
                             }
                         }
+
+                        return;
                     });
                 });
 
