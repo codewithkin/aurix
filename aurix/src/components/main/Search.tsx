89e76fda3e208f57e6876dbbfe4b7ee6d647a6b7
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-08 11:32:30 +0200

    Refactor Search component to handle search queries and submit functionality

--- aurix/src/components/main/Search.tsx
+++ aurix/src/components/main/Search.tsx
@@ -1,20 +1,23 @@
-import React from "react";
+import { useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
-function Search() {
+function Search({ searchFn, searching }: {  searchFn: any; searching: boolean }) {
+  const [query, setQuery] = useState("");
+
   return (
-    <article className="flex gap-2 items-center w-full">
+    <form onSubmit={() => searchFn(query)} className="flex gap-2 items-center w-full">
       <Input
         className="py-6 px-4 focus-visible:ring-sky-400 focus-visible:ring-1 focus-visible:outline-none focus-visible:border-0 rounded-lg"
         type="search"
+        onChange={(e) => setQuery(e.target.value)}
         placeholder="Search for a gig keyword..."
       />
 
-      <Button variant="default" className="bg-sky-400 text-white p-6">
+      <Button type="submit" variant="default" className="bg-sky-400 text-white p-6">
         Find Gigs
       </Button>
-    </article>
+    </form>
   );
 }
 
