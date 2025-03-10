bb179dd57fa0385714d2c3b6a0ebf1e748e4b8d0
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-10 13:16:04 +0200

    Refactor auth page layout for improved user experience and visual appeal

--- aurix/src/app/auth/page.tsx
+++ aurix/src/app/auth/page.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
 function page() {
-    throw new Error("Blah")
 
   return (
-    <div>
-      
-    </div>
+    <section className="bg-gradient-to-tr from-sky-200 to-blue-500 min-h-screen min-w-screen transition-all duration-500 hover:from-blue-500 hover:to-sky-200 flex flex-col justify-center items-center">
+      {/* Form */}
+      <form className="bg-transparent bg-white p-8 border-2 border-purple-600 rounded-xl w-fit h-fit" action="">
+        <article className='text-center flex flex-col items-center justify-center'>
+          <h2 className="font-semibold text-2xl bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-500">Welcome Back</h2>
+          <p className='text-slate-600'>Let's get you signed in</p>
+        </article>
+
+        {/* Fields */}
+
+      </form>
+    </section>
   )
 }
 
