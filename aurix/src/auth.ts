5320bdb58d6f225ac453c8791bb78a7abd33c92e
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-10 14:06:47 +0200

    Add Prisma schema and client setup for database integration

--- aurix/src/auth.ts
+++ aurix/src/auth.ts
@@ -1,8 +1,11 @@
 import NextAuth from "next-auth";
 import Reddit from "next-auth/providers/reddit";
 import Resend from "next-auth/providers/resend";
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
   providers: [
     Resend,
     Reddit
