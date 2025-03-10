bbf63269a16e2497bdb1e336a2f3fa36875d83a8
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-10 12:25:38 +0200

    Refactor NotFound component to improve text size and responsiveness for better user experience

--- aurix/src/app/not-found.tsx
+++ aurix/src/app/not-found.tsx
@@ -7,8 +7,8 @@ function NotFound() {
     <div className="flex h-screen items-center justify-center">
       <div className="text-center flex flex-col gap-4 items-center justify-center">
         <article>
-          <h1 className="text-6xl font-bold">Ooops, <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-500 block">Nothing here</span></h1>
-          <p className="text-xl text-slate-600">Sorry, the page you were looking for couldn't be found</p>
+          <h1 className="text-4xl md:text-6xl font-bold">Ooops, <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-500 block">Nothing here</span></h1>
+          <p className="md:text-xl text-slate-600">Sorry, the page you were looking for couldn't be found</p>
         </article>
         
         <Button className="bg-gradient-to-tr from-purple-600 to-blue-500 flex gap-2 hover:gap-4 w-fit transition duration-300" asChild variant="default">
