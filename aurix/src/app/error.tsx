3f5b5a8f8a81146dc68f9c58799be8375dd51eb0
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-10 10:21:16 +0200

    Enhance error icon styling with updated size and color

--- aurix/src/app/error.tsx
+++ aurix/src/app/error.tsx
@@ -7,7 +7,7 @@ function ErrorPage({ error, reset }: { error: { message: string }, reset: () => 
 
   return (
     <section className="min-h-screen w-full flex flex-col justify-center items-center text-center">
-        <Bug size={32} />
+        <Bug className="text-purple-700" strokeWidth={1.5} size={72} />
         <h2 className="text-4xl md:text-6xl font-semibold">Ooops</h2>
         <p className="text-slate-600 text-lg">An error occured, please try again later</p>
         <article className="bg-gray-900 hover:cursor-pointer border-2 border-purple-600 rounded-md my-2 text-white py-2 px-4 hover:border-orange-600 hover:shadow-xl transition duration-300 flex gap-2 items-center">
