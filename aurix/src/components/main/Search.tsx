d048eb8491449878b446ebf1904801455719dc5a
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-10 10:57:42 +0200

    Refactor Search and ErrorPage components for improved readability and structure

--- aurix/src/components/main/Search.tsx
+++ aurix/src/components/main/Search.tsx
@@ -2,15 +2,24 @@ import { useState } from "react";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 
-function Search({ searchFn, searching }: {  searchFn: any; searching: boolean }) {
+function Search({
+  searchFn,
+  searching,
+}: {
+  searchFn: any;
+  searching: boolean;
+}) {
   const [query, setQuery] = useState("");
 
   return (
-    <form onSubmit={(e) => {
-      e.preventDefault();
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
 
-      searchFn(query)
-    }} className="flex gap-2 items-center w-full">
+        searchFn(query);
+      }}
+      className="flex gap-2 items-center w-full"
+    >
       <Input
         className="py-6 px-4 focus-visible:ring-sky-400 focus-visible:ring-1 focus-visible:outline-none focus-visible:border-0 rounded-lg"
         type="search"
@@ -18,7 +27,11 @@ function Search({ searchFn, searching }: {  searchFn: any; searching: boolean })
         placeholder="Search for a gig keyword..."
       />
 
-      <Button type="submit" variant="default" className="bg-sky-400 text-white p-6">
+      <Button
+        type="submit"
+        variant="default"
+        className="bg-sky-400 text-white p-6"
+      >
         Find Gigs
       </Button>
     </form>
