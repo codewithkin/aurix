8c71d1452c7f5540d66bda2b76c58ba7a589a5df
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-10 10:11:55 +0200

    Enhance error page styling with improved text alignment and transition effects

--- aurix/src/app/error.tsx
+++ aurix/src/app/error.tsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 function ErrorPage({ error, reset }: { error: { message: string }, reset: () => void }) {
 
   return (
-    <section className="min-h-screen w-full flex flex-col justify-center items-center">
-        <h2 className="text-4xl font-semibold">Ooops</h2>
+    <section className="min-h-screen w-full flex flex-col justify-center items-center text-center">
+        <h2 className="text-4xl md:text-6xl font-semibold">Ooops</h2>
         <p className="text-slate-600 text-lg">An error occured, please try again later</p>
-        <p className="bg-gray-900 hover:cursor-pointer border-2 border-purple-600 rounded-md my-2 text-white py-2 px-8 hover:border-orange-600 hover:shadow-sm transitino duration-500">
+        <p className="bg-gray-900 hover:cursor-pointer border-2 border-purple-600 rounded-md my-2 text-white py-2 px-8 hover:border-orange-600 hover:shadow-xl transition duration-300">
             {error.message}
         </p>
         <article className="flex md:flex-row flex-col gap-4 items-center justify-center w-full">
