099e329458d98f4503a7ea78f45e370573a39bf1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-10 10:20:55 +0200

    [ADD]: Include icons for error indication and enhance error message display

--- aurix/src/app/error.tsx
+++ aurix/src/app/error.tsx
@@ -1,16 +1,19 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Bug, Code } from "lucide-react";
 import Link from "next/link";
 
 function ErrorPage({ error, reset }: { error: { message: string }, reset: () => void }) {
 
   return (
     <section className="min-h-screen w-full flex flex-col justify-center items-center text-center">
+        <Bug size={32} />
         <h2 className="text-4xl md:text-6xl font-semibold">Ooops</h2>
         <p className="text-slate-600 text-lg">An error occured, please try again later</p>
-        <p className="bg-gray-900 hover:cursor-pointer border-2 border-purple-600 rounded-md my-2 text-white py-2 px-8 hover:border-orange-600 hover:shadow-xl transition duration-300">
-            {error.message}
-        </p>
+        <article className="bg-gray-900 hover:cursor-pointer border-2 border-purple-600 rounded-md my-2 text-white py-2 px-4 hover:border-orange-600 hover:shadow-xl transition duration-300 flex gap-2 items-center">
+            <Code size={18} />
+            <p>{error.message}</p>
+        </article>
         <article className="flex md:flex-row flex-col gap-4 items-center justify-center w-full">
             <Button onClick={reset} className="bg-purple-600 text-white font-semibold" variant="default">
                 Reload
