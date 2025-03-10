8833e423333204e3913af60128eb59f9cb435400
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-10 09:37:00 +0200

    [ADD]: Error page

---
+++ aurix/src/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+function ErrorPage() {
+  return (
+    <section className="min-h-screen w-full flex flex-col justify-center items-center">
+        <h2>Ooops</h2>
+        <p>An error occured, please try again later</p>
+    </section>
+  )
+}
+
+export default ErrorPage
