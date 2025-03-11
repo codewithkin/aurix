c41dfe1df34215aade6af0189d647329e5a2c3f3
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-11 12:50:17 +0200

    Refactor authentication setup to include Resend and Google providers; create Prisma client instance

--- aurix/src/auth.ts
+++ aurix/src/auth.ts
@@ -1,13 +1,30 @@
-import NextAuth from "next-auth"
- 
-import { PrismaClient } from "@prisma/client"
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import authConfig from "./auth.config"
- 
-const prisma = new PrismaClient()
- 
+import NextAuth from "next-auth"
+import Resend from "next-auth/providers/resend"
+import GoogleProvider from "next-auth/providers/google"
+import { prisma } from "./prisma"
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" },
-  ...authConfig,
+  providers: [
+    Resend,
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  pages: {
+    signIn: '/auth',
+  },
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return '/'
+    },
+    async session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id
+      }
+      return session
+    }
+  }
 })
