3bcc61d15155b44589750e9c09aabca95310066e
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-11 12:21:54 +0200

    Replace GitHub provider with Resend and Reddit in authentication configuration

--- aurix/src/auth.config.ts
+++ aurix/src/auth.config.ts
@@ -1,7 +1,8 @@
-import GitHub from "next-auth/providers/github"
 import type { NextAuthConfig } from "next-auth"
+import Reddit from "next-auth/providers/reddit"
+import Resend from "next-auth/providers/resend"
  
 // Notice this is only an object, not a full Auth.js instance
 export default {
-  providers: [GitHub],
+  providers: [Resend, Reddit],
 } satisfies NextAuthConfig
