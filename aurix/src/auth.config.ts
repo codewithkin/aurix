c8315b2aa1e4b76b8f1af3242d57f19c3fc3cc00
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-11 12:15:49 +0200

    [DELETE]: Old project

--- aurix/src/auth.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextAuthConfig } from "next-auth"
-import Reddit from "next-auth/providers/reddit";
-import Resend from "next-auth/providers/resend";
-
-export default {
-    providers: [Resend, Reddit],
-} satisfies NextAuthConfig;
