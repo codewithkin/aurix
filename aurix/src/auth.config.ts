1830589eab21928b6f84bf0dcf1a0c8d4900e0d4
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-03-14 08:59:16 +0200

    Minor changes

--- aurix/src/auth.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextAuthConfig } from "next-auth"
-import Reddit from "next-auth/providers/reddit"
-import Resend from "next-auth/providers/resend"
- 
-// Notice this is only an object, not a full Auth.js instance
-export default {
-  providers: [Resend, Reddit],
-} satisfies NextAuthConfig
